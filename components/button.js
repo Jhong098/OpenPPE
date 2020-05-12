efdfabec5bc99289ebfe81f0325105dc549e49fa
Steven Steven <s3@uwaterloo.ca>
2020-05-12 17:33:03 -0400

    add hove effect

--- components/button.js
+++ components/button.js
@@ -1,6 +1,6 @@
 export default function Button({href, children}) {
   return (
-    <button href={href} className="bg-primary text-primary_text py-2 px-4 mx-2 rounded">
+    <button href={href} className="bg-primary hover:bg-dark_primary text-primary_text py-2 px-4 mx-2 rounded">
         {children}
     </button>
   );
