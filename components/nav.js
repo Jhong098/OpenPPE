1162d7857dabeb0790d69e3f65cce158bf4fef12
Steven Steven <s3@uwaterloo.ca>
2020-05-12 21:09:05 -0400

    Add Profile Pic

--- components/nav.js
+++ components/nav.js
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import Button from "components/button"
+import {useState} from "react"
+import { Bell } from 'react-feather';
 
 const links = [
   { href: "/", label: "Home" },
@@ -7,7 +9,37 @@ const links = [
   { href: "/requests", label: "Requests" },
 ];
 
-export default function Nav() {
+export default function Nav({auth = true}) {
+  const [profileSelected, setProfileSelected] = useState(false)
+
+  const rightOptions = () => {
+    if (auth){
+      return (
+        <div class="flex justify-center items-center">
+          <Bell/>
+          <div class="relative pl-8">
+            <button onClick={()=>setProfileSelected(!profileSelected)} class="relative z-20 h-8 w-8 rounded-full overflow-hidden border-2 border-gray-600 focus:outline-none focus:border-white">
+              <img class="h-full w-full object-cover" src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&q=80" alt="Your avatar"/>
+            </button>
+            <button onClick={()=>setProfileSelected(false)} class="fixed inset-0 h-full w-full bg-black opacity-0 cursor-default"></button>
+            { profileSelected ? (
+              <div class="absolute right-0 mt-2 py-2 w-48 bg-primary rounded-lg shadow-xl">
+                <a href="#" class="block px-4 py-2 hover:bg-dark_primary">Account settings</a>
+                <a href="#" class="block px-4 py-2 hover:bg-dark_primary">Sign out</a>
+              </div>
+            ):null}
+          </div>
+        </div>
+      )
+    } else {
+        return (
+            <div>
+                <Button href='#'>Signup</Button>
+                <Button href='#'>Login</Button>
+            </div>
+        )
+    }
+  }
   return (
     <nav className="flex flex-wrap justify-between items-center text-white bg-dark_primary px-8 h-16">
       <ul className="flex items-center">
@@ -19,10 +51,7 @@ export default function Nav() {
             </li>
         ))}
       </ul>
-      <div>
-        <Button href='#'>Signup</Button>
-        <Button href='#'>Login</Button>
-      </div>
+      {rightOptions()}
     </nav>
   );
 }
