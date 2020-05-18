623bdcfbbc77226737b15c32d867fa97dbf44392
Jason Hong <jhong098@gmail.com>
2020-05-18 02:49:52 -0400

    try again

--- components/requestGrid.jsx
+++ components/requestGrid.jsx
@@ -31,7 +31,7 @@ export default function RequestGrid({
         )}
       </AnimatePresence>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-4">
         {data.map((item, i) => (
           <div key={i} onClick={() => setSelectedCard(i)}>
             <Card data={item} />
